c6e76466f1b74601cef6c9ce6b748e5da4b3510c
Cristian Castillo <crianbluff77@gmail.com>
2019-07-10 08:22:47 -0500

    Función de visualizar en pantalla grande la imagen

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -33,13 +33,37 @@ export class FotosComponent implements OnInit {
     this.afs.collection('img').snapshotChanges()
     .subscribe( res => {
       this.images = res;
-    }); 
+    });
+  }
+
+  verImagen(event) {
+    const modal = document.getElementById('modal-lightbox');
+    let imgModal = modal.querySelector('.ctn-modal-lightbox .modal-lightbox-body img');
+    let pathImage = event.target.parentElement.parentElement.children[1].src;
+    imgModal.setAttribute('src', pathImage)
+    modal.classList.add('show-modal-lightbox');
+    document.body.classList.add('no-scroll-y');
+  }
+
+  cerrarModalClickFuera(event) {
+    let temp = event.target;
+    temp.classList.remove('show-modal-lightbox');
+    document.body.classList.remove('no-scroll-y');
+  }
+
+  stopPropagation(event) {
+    event.stopPropagation();
+  }
+
+  closeModal() {
+    const modal = document.getElementById('modal-lightbox');
+    modal.classList.remove('show-modal-lightbox');
+    document.body.classList.remove('no-scroll-y');
   }
 
   eliminarImagen(event) {
     let key = event.target.id;
     this._cargaImagenes.eliminarImagen(key);
-
   }
 
   ngOnInit() {
